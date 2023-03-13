35344c2805dcd1655841be6615633d16bb903c7f
HelloIverson <102616794+HelloIverson@users.noreply.github.com>
2023-03-13 08:47:56 -0600

    Update gui.py

--- src/src/gui.py
+++ src/src/gui.py
@@ -1,50 +1,85 @@
-from tkinter import *
 import tkinter as tk
-import datetime
+from tkinter import ttk
 
-class menuButton(tk.Button):
-    
-    def PresentWindow():
-        window = Toplevel()
-        window.geometry('400x400')
-        newlabel = Label(window, text = "How do you feel today?")
-        newlabel.pack()
-    def PastWindow():
-        window = Toplevel()
-        window.geometry('400x400')
-        newlabel = Label(window, text = "Here are some of your  best days")
-        newlabel.pack()
-    def SettingWindow():
-        window = Toplevel()
-        window.geometry('400x400')
-        newlabel = Label(window, text = "Settings Window")
-        newlabel.pack()
-    def ExitApp():
-        root.destroy()
- 
-root = Tk()
-root.geometry('500x500')
- 
-frame = Frame(root, borderwidth= 2, relief= 'sunken')
-frame.pack()
+LARGE_FONT= ("Verdana", 12)
 
-launch = Label(frame, text= "You launched at:")
-launch.pack()
 
-time = Label(frame, text= datetime.datetime.today())
-time.pack()
+style = ttk.Style()
+style.configure("Custom.TButton",
+                 foreground="white",
+                 background="black",
+                 padding=[10, 10, 10, 10],
+                 font="Verdana 12 underline")
+class App(tk.Tk):
 
-currentButton = menuButton(frame, text= "Today", command=menuButton.PresentWindow)
-currentButton.pack(pady=10)
+    def __init__(self, *args, **kwargs):
+        
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
 
-pastButton = menuButton(frame, text= "Past", command= menuButton.PastWindow)
-pastButton.pack(pady=10)
+        container.pack(side="top", fill="both", expand = True)
 
-optionButton = menuButton(frame, text = "Options", command = menuButton.SettingWindow)
-optionButton.pack(pady = 10)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
-exitButton = menuButton(frame, text= "Quit", command= menuButton.ExitApp)
-exitButton.pack(pady = 10)
+        self.title("Deez")
+        self.geometry("500x500")
 
-root.title("App")
-root.mainloop()
+        self.frames = {}
+
+        for F in (HomePage, PageOne, PageTwo):
+
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(HomePage)
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
+
+        
+class HomePage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self,parent)
+        label = ttk.Label(self, text="Main Menu", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button = ttk.Button(self, text="Today", style="Custom.TButton",
+                            command=lambda: controller.show_frame(PageOne))
+        button.pack()
+
+        button2 = ttk.Button(self, text="Past Days", style="Custom.TButton", 
+                            command=lambda: controller.show_frame(PageTwo))
+        button2.pack()
+
+
+class PageOne(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="How are you today?", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button1 = ttk.Button(self, text="Main Menu",
+                            command=lambda: controller.show_frame(HomePage))
+        button1.pack()
+
+class PageTwo(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Look back on your best days.", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button1 = ttk.Button(self, text="Main Menu",
+                            command=lambda: controller.show_frame(HomePage))
+        button1.pack()
+
+app = App()
+app.mainloop()
